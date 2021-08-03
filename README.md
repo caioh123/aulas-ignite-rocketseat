a55635b7810177029e48ed82edbeea8cfc60a70c
Caio Novaes <caionwebdev@gmail.com>
2021-08-03 01:27:01 -0300

    aula 3-1

--- README.md
+++
@@ -1,2 +0,0 @@
-# aulas-ignite-rocketseat
-Bootcamp
---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,22 @@
+const repositoryName = "unform";
+import { RepositoryItem } from "./RepositoryItem";
+
+const repository = {
+  name: "unform",
+  description: "Forms in react",
+  link: "https://github.com/unform/unform",
+};
+
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositório</h1>
+
+      <ul>
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+      </ul>
+    </section>
+  );
+}
